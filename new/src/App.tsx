f9a174a29b7e30521bafe1a330e938429d601f6d
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2026-06-25 22:11:26 -0300

    config new projeto com tailwindcss

---
+++ new/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <section className="text-center">
+        <h1 className="text-4xl font-bold text-blue-400">
+          React + Vite + TypeScript + Tailwind CSS 4
+        </h1>
+
+        <p className="mt-4 text-zinc-300">
+          Projeto configurado com sucesso.
+        </p>
+
+        <button className="mt-6 rounded-lg bg-blue-500 px-5 py-3 font-medium text-white hover:bg-blue-600">
+          Começar
+        </button>
+      </section>
+    </main>
+  )
+}
+
+export default App
